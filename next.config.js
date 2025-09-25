693ca49a4a792f9ea7e6c2ad5935b6510ed4a6ba
r91781585-tech <r91781585@gmail.com>
2025-09-25 20:24:42 +0530

    Update Next.js config for external image optimization

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
   },
+  experimental: {
+    optimizeCss: true,
+  }
 }
 
 module.exports = nextConfig
